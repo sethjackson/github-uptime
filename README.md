19369d704650210bc394010ba5a493e8ff039aad
Seth Jackson <sethjackson@gmail.com>
2026-08-28 22:01:34 -0400

    Use GitHub status API directly

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Github Uptime
 
-This is a silly site designed in a silly way for silly purposes.
+This is a silly site created for silly purposes.
 You should not rely on it and it may stop working at any time.
 Go [here](https://sethjackson.github.io/github-uptime/) to see it live.
 
--- js/app.js
+++ js/app.js
@@ -2,15 +2,43 @@ document.addEventListener('DOMContentLoaded', async () => {
   const uptimeElement = document.getElementById('uptime');
 
   try {
-    const response = await fetch('https://ghu-function.azurewebsites.net/api/uptime');
+    const unresolvedIncidentsResponse = await fetch('https://www.githubstatus.com/api/v2/incidents/unresolved.json');
 
-    if (!response.ok) {
-      throw new Error('Unable to retrieve GitHub status: ${response.status}');
+    if (!unresolvedIncidentsResponse.ok) {
+      throw new Error(`Unable to retrieve unresolved GitHub incidents: ${unresolvedIncidentsResponse.status}`);
     }
 
-    const result = await response.json();
+    const unresolvedIncidentsResult = await unresolvedIncidentsResponse.json();
 
-    const days = result['days'];
+    let incident = null;
+    if (unresolvedIncidentsResult.incidents.length === 0) {
+      const incidentsResponse = await fetch('https://www.githubstatus.com/api/v2/incidents.json');
+
+      if (!incidentsResponse.ok) {
+        throw new Error(`Unable to retrieve GitHub incidents: ${incidentsResponse.status}`);
+      }
+
+      const incidentsResult = await incidentsResponse.json();
+
+      if (incidentsResult.incidents.length === 0) {
+        throw new Error('No incident data in GitHub status API response.');
+      }
+
+      incident = incidentsResult.incidents[0];
+    } else {
+      incident = unresolvedIncidentsResult.incidents[0];
+    }
+
+    let outage = null;
+    if (incident['resolved_at']) {
+      outage = new Date(incident['resolved_at']);
+    } else if (incident['started_at']) {
+      outage = new Date(incident['started_at']);
+    } else {
+      outage = new Date(incident['created_at']);
+    }
+
+    const days = Math.floor((Date.now() - outage) / (1000 * 60 * 60 * 24));
 
     if (days > 0) {
       uptimeElement.className = 'up';
